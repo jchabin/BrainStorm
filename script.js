4e455f5142fb9023f6abd7e4ac5a624b01b1623c
jchabin <jchabin@cherrycreekschools.org>
2017-01-23 16:45:53 -0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,8 @@
+function button(){
+	var stuff=new XMLHttpRequest();
+	stuff.onreadystatechange=function(){
+		document.getElementById("return").innerHTML=this.responseText;
+	}
+	stuff.open("GET","chat.php?s=1234");
+	stuff.send();
+}
