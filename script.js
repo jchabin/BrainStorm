0149760939c4cfd469eb5df5c1d64b3b3c02038c
jchabin <jchabin@cherrycreekschools.org>
2017-01-23 17:05:37 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,7 +1,8 @@
 function button(){
 	var stuff=new XMLHttpRequest();
 	stuff.onreadystatechange=function(){
-		console.log(this.responseText);
+		if (this.readyState == 4 && this.status == 200)
+			console.log(this.responseText);
 	}
 	stuff.open("GET","chat.php?s=1234",true);
 	stuff.send();
